185ee6c5334a2054b68643072c52a55316b7f8dc
RakSridhar23 <rss9243@nyu.edu>
2024-03-26 12:31:33 -0400

    removing irrelevant files

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dotenv import load_dotenv
-from pytify.client import get_artist_id, get_track_id, authenticate
+from pytify.client import get_artist_id, authenticate
 
 
 
