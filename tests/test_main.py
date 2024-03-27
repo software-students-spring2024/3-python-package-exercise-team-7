633788180b4ee20a025dc3a15f96e2b808487e36
nhaulsey <ndhaulsey@gmail.com>
2024-03-26 23:09:30 -0400

    Fixed bug that caused the package and tests to crash.

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dotenv import load_dotenv
-from src.pytify_stats.client import Client
+from pytify_stats.client import Client
 
 class Tests:
 
