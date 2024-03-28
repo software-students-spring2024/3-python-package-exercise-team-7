884a35e6ebab0343481ad0343d76ea6ebe0a7119
RakSridhar23 <rss9243@nyu.edu>
2024-03-28 15:25:19 -0400

    changed name to pytify_stats

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from dotenv import load_dotenv
-from pytify.client import Client
+from pytify_stats.client import Client
 
 
 
